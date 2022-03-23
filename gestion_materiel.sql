146571b8dcf15d65e0d71163f704739c71a45089
ntet2001 <manyamaigor2001@gmail.com>
2022-03-23 13:58:31 +0100

    database file modified

---
+++ gestion_materiel.sql
@@ -0,0 +1,214 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 23 mars 2022 à 13:57
+-- Version du serveur :  8.0.21
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gestion_materiel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int NOT NULL AUTO_INCREMENT,
+  `nom_client` varchar(255) NOT NULL,
+  `prenom_client` varchar(255) NOT NULL,
+  `adresse_client` varchar(255) NOT NULL,
+  `email_client` varchar(255) DEFAULT NULL,
+  `dateclient` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_client`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id_commande` int NOT NULL AUTO_INCREMENT,
+  `nom_commande` varchar(255) NOT NULL,
+  `client` int NOT NULL,
+  `datecommande` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_commande`),
+  KEY `client` (`client`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `facture`
+--
+
+DROP TABLE IF EXISTS `facture`;
+CREATE TABLE IF NOT EXISTS `facture` (
+  `id_facture` int NOT NULL AUTO_INCREMENT,
+  `lignecommande` int NOT NULL,
+  `datesortie` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_facture`),
+  KEY `lignecommande` (`lignecommande`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fournisseur`
+--
+
+DROP TABLE IF EXISTS `fournisseur`;
+CREATE TABLE IF NOT EXISTS `fournisseur` (
+  `id_fournisseur` int NOT NULL AUTO_INCREMENT,
+  `nom_fournisseur` varchar(255) NOT NULL,
+  `adresse_fournisseur` varchar(255) NOT NULL,
+  `telephone` varchar(30) NOT NULL,
+  `datefournisseur` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_fournisseur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lignecommande`
+--
+
+DROP TABLE IF EXISTS `lignecommande`;
+CREATE TABLE IF NOT EXISTS `lignecommande` (
+  `id_lignecommande` int NOT NULL AUTO_INCREMENT,
+  `commande` int NOT NULL,
+  `materiel` int NOT NULL,
+  `datelignecommande` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `statut` int DEFAULT NULL,
+  `qtecommande` int NOT NULL,
+  `montant` int NOT NULL,
+  PRIMARY KEY (`id_lignecommande`),
+  KEY `commande` (`commande`,`materiel`),
+  KEY `fk_ligne_materiel` (`materiel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livraison`
+--
+
+DROP TABLE IF EXISTS `livraison`;
+CREATE TABLE IF NOT EXISTS `livraison` (
+  `id_livraison` int NOT NULL AUTO_INCREMENT,
+  `fournisseur` int NOT NULL,
+  `materiel` int NOT NULL,
+  `datelivraison` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `qtelivrer` int NOT NULL,
+  PRIMARY KEY (`id_livraison`),
+  KEY `fournisseur` (`fournisseur`,`materiel`),
+  KEY `fk_livraison_materiel` (`materiel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `materiel`
+--
+
+DROP TABLE IF EXISTS `materiel`;
+CREATE TABLE IF NOT EXISTS `materiel` (
+  `id_materiel` int NOT NULL AUTO_INCREMENT,
+  `nom_materiel` varchar(255) NOT NULL,
+  `prixUnitaire` int NOT NULL,
+  `caracteristique` text NOT NULL,
+  `qte_init` int NOT NULL,
+  `datemateriel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `typemateriel` int NOT NULL,
+  PRIMARY KEY (`id_materiel`),
+  KEY `typemateriel` (`typemateriel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_materiel`
+--
+
+DROP TABLE IF EXISTS `type_materiel`;
+CREATE TABLE IF NOT EXISTS `type_materiel` (
+  `id_typemateriel` int NOT NULL AUTO_INCREMENT,
+  `marque` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_typemateriel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_utilisateur` int NOT NULL AUTO_INCREMENT,
+  `nom_utilisateur` varchar(255) NOT NULL,
+  `email_utilisateur` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `dateutilisateur` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `fk_client_commande` FOREIGN KEY (`client`) REFERENCES `client` (`id_client`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD CONSTRAINT `fk_facture_ligne` FOREIGN KEY (`lignecommande`) REFERENCES `lignecommande` (`id_lignecommande`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `lignecommande`
+--
+ALTER TABLE `lignecommande`
+  ADD CONSTRAINT `fk_ligne_commande` FOREIGN KEY (`commande`) REFERENCES `commande` (`id_commande`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_ligne_materiel` FOREIGN KEY (`materiel`) REFERENCES `materiel` (`id_materiel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `livraison`
+--
+ALTER TABLE `livraison`
+  ADD CONSTRAINT `fk_livraison_fournisseur` FOREIGN KEY (`fournisseur`) REFERENCES `fournisseur` (`id_fournisseur`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_livraison_materiel` FOREIGN KEY (`materiel`) REFERENCES `materiel` (`id_materiel`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `materiel`
+--
+ALTER TABLE `materiel`
+  ADD CONSTRAINT `fk_materiel_typemateriel` FOREIGN KEY (`typemateriel`) REFERENCES `type_materiel` (`id_typemateriel`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
